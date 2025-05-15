00dddc328f6221b9f0e6ec6ee4a564b5a8d013ac
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2025-05-15 08:14:25 +0530

    update

--- notes.js
+++ notes.js
@@ -1,3 +1,9 @@
 // abstraction-  means main behavaiur se matlb ho andr kya ho rha h janne ki jarurat hi  nhi h jse if else for isme andr kya ho rha h muje nhi jnnna ye bhi ak abstractin hai. data hinding
-
+// https://github.com/adityatandon15/LLD
 // Encapsulation-  data security
+
+// polymorphysm-   ak metgod km alag , 
+// dynamic- 3 alag run () alg logo ke liye alg km krenge
+// stattuc - run (int speed)  speed ke hisab se alg sitiation me alg trh se bhgega.
+
+// what is operator-overloading in c++ kyoki aur kisi me nhi hota h ye.
