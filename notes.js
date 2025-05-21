b83dfca1804017885f4669fdd26966f7bbc48b45
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2025-05-21 08:29:37 +0530

    solid  i   and d principle

--- notes.js
+++ notes.js
@@ -7,3 +7,12 @@
 // stattuc - run (int speed)  speed ke hisab se alg sitiation me alg trh se bhgega.
 
 // what is operator-overloading in c++ kyoki aur kisi me nhi hota h ye.
+// imutable class- final  lga do class ke age immutable bn jega and  jise koi inherit na kr paye
+// imutable method- final  lga do method ke age immutable bn jega and  jise koi inherit na kr paye
+
+
+
+// isp- shape 2 alag alg banne pdenge 2d and 3d  kykki bhut se me area  volume nhi hota h
+
+//dependency -injection:-object pass kiya  as a variable kisi class me use bolte h dependency injection
+// if open close priciple is target , dependendency inversion is a solution.
